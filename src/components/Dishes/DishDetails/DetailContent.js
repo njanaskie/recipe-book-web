29e883ee3f46a6917b597ea514c4df243f5328cb
Nick <njanaskie@gmail.com>
2020-06-16 22:04:25 -0400

    use link preview package

--- src/components/Dishes/DishDetails/DetailContent.js
+++ src/components/Dishes/DishDetails/DetailContent.js
@@ -1,37 +1,34 @@
 import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import FirebaseContext from '../../../../context/firebase-context'
+import { ReactTinyLink } from 'react-tiny-link'
 
 const DetailContent = (dish) => {
     const { isAdmin } = useContext(FirebaseContext)
 
-    // useEffect(() => {
-    const getDataFromURL = (dish) => {
-        if (dish) {
-            console.log('fetching ', dish.recipes)
-            return dish.recipes.map(recipe =>
-                fetch(`${recipe}`)
-                    .then((response) => response.json())
-                    .then((responseJson) => {
-                    //   return responseJson.movies;
-                        console.log(responseJson)
-                    })
-                    .catch((error) => {
-                        console.error(error);
-                    })
-                );
-        } else {
-            console.log('not fetching')
-        }
-    }
+    // const getDataFromURL = (dish) => {
+    //     if (dish) {
+    //         console.log('fetching ', dish.recipes)
+    //         return dish.recipes.map(recipe =>
+    //             fetch(`${recipe}`)
+    //                 .then((response) => response.json())
+    //                 .then((responseJson) => {
+    //                 //   return responseJson.movies;
+    //                     console.log(responseJson)
+    //                 })
+    //                 .catch((error) => {
+    //                     console.error(error);
+    //                 })
+    //             );
+    //     } else {
+    //         console.log('not fetching')
+    //     }
+    // }
 
-
-    // }, [])
-
-    if (dish.id) {
-        console.log(dish)
-        getDataFromURL(dish)
-    }
+    // if (dish.id) {
+    //     console.log(dish)
+    //     getDataFromURL(dish)
+    // }
 
     return (
         <div>
@@ -44,7 +41,8 @@ const DetailContent = (dish) => {
             <h4>Optional Ingredients</h4>
             {dish.optionalIngredients && dish.optionalIngredients.map(optionalIngredient => <li key={optionalIngredient}>{optionalIngredient}</li>)}
             <h3>Recipes</h3>
-            {dish.recipes && dish.recipes.map(recipe => <p key={recipe}><a href={recipe} target="_blank" rel="noopener noreferrer">{recipe}</a></p>)}
+            {/* {dish.recipes && dish.recipes.map(recipe => <p key={recipe}><a href={recipe} target="_blank" rel="noopener noreferrer">{recipe}</a></p>)} */}
+            {dish.recipes && dish.recipes.map(recipe => <ReactTinyLink key={recipe} url={recipe}>{recipe}</ReactTinyLink>)}
             {isAdmin && <Link to={`/edit/dish/${dish.id}`}>Edit Dish</Link>}
         </div>
     )
