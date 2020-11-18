747bb23aac4918364f9cfefee1dc1597f817ca13
Nick <njanaskie@gmail.com>
2020-11-17 23:36:03 -0500

    strengthen ingredients filter logic

--- src/selectors/recipes.js
+++ src/selectors/recipes.js
@@ -1,6 +1,6 @@
 export default (recipes, filters) => {
     return filters ? recipes.filter((recipe) => {
-        const ingredientMatch = recipe.ingredients.some(ingredient => ingredient.includes(filters.ingredients))
+        const ingredientMatch = filters.ingredients.length > 0 ? !recipe.ingredients.every(ingredient => !filters.ingredients.includes(ingredient)) : true
         const cuisineMatch = recipe.cuisine.toLowerCase().includes(filters.cuisine.toLowerCase())
         const typeMatch = recipe.type.toLowerCase().includes(filters.recipeType.toLowerCase())
         const customTagMatch = filters.customTags.length > 0 ? recipe.customTags.some(tag => tag.includes(filters.customTags)) : true
