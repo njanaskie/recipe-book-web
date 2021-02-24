7cea69ce18c9a9243d7b28d63a4b49349e65f214
Nick <njanaskie@gmail.com>
2021-02-23 23:30:25 -0500

    fix server.js

--- src/server/server.js
+++ src/server/server.js
@@ -24,9 +24,13 @@ app.use(cors());
 app.use(express.json());
 app.use(decodeIDToken);
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(publicPath, 'index.html'));
-// });
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(publicPath));
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(publicPath, 'index.html'));
+  });
+}
 
 app.use('/api', router);
 
